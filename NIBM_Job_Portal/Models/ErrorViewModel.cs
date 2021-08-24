43d799d54b1447e9953242de050d53756594eeb0
ashan.dias <ashan.dias@hitachi-dps.com>
2021-08-24 23:42:29 +0530

    added

---
+++ NIBM_Job_Portal/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NIBM_Job_Portal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
